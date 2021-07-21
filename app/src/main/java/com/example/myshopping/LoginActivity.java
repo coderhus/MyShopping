67d8c4191c2eb94f35b92c7467df4a8cefd87893
leminhdung2701 <leminhdung_t64@hus.edu.vn>
2021-07-21 12:33:51 +0700

    Đây nhá

--- app/src/main/java/com/example/myshopping/LoginActivity.java
+++ app/src/main/java/com/example/myshopping/LoginActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
+import android.text.method.SingleLineTransformationMethod;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     TextView register_txt;
+    EditText password_edt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +30,26 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        password_edt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (password_edt.getTransformationMethod().getClass().getSimpleName() .equals("PasswordTransformationMethod")) {
+                    password_edt.setTransformationMethod(new SingleLineTransformationMethod());
+                }
+                else {
+                    password_edt.setTransformationMethod(new PasswordTransformationMethod());
+                }
+
+                password_edt.setSelection(password_edt.getText().length());
+            }
+        });
+        
+        
     }
 
     private void AnhXa() {
         register_txt = (TextView)findViewById(R.id.register);
+        password_edt = (EditText) findViewById(R.id.password);
     }
 }
