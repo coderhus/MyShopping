bbbd91288a475bf6a286e7518d658d5dd5b800e3
ADMIN <coderhus@gmail.com>
2021-07-28 12:04:46 +0700

    sửa lại 1 chút noti

---
+++ app/src/main/java/com/example/myshopping/Constants/Constants.java
@@ -0,0 +1,16 @@
+package com.example.myshopping.Constants;
+
+public interface Constants {
+
+    String VERIFICATION_CODE = "code";
+    String IMAGE_PATH = "Media/Profile_Image/profile";
+    int STORAGE_REQUEST_CODE = 1000;
+    int USERNAME_CODE = 100;
+    int CONTACTS_REQUEST_CODE = 2000;
+    int RECORDING_REQUEST_CODE = 3000;
+    String CHANNEL_ID = "1000";
+    int NOTIFICATION_ID = 100;
+
+    String NOTIFICATION_URL = "https://fcm.googleapis.com/fcm/send";
+    String SERVER_KEY ="AAAAkkX0CVs:APA91bEQv-k3ZRIEY7hP4qYyxBhjlU7oHBlTNqzzOLHQmAjY0nY9QVAMLeQoGdNeJYNltZ2IHAG2PMfH3WNp4S2W4ARPplBqVQyrDNfW_el6pqqGkeYHmwk65CRSpMl5xZ46REfsKVCu";
+}
