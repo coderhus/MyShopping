516276d59693ae92f29432da2ca3e2b19768e4f6
leminhdung2701 <leminhdung_t64@hus.edu.vn>
2021-09-02 21:29:34 +0700

    new

---
+++ app/src/main/java/com/example/myshopping/Activity/InformationActivity.java
@@ -0,0 +1,16 @@
+package com.example.myshopping.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myshopping.R;
+
+public class InformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information);
+    }
+}
