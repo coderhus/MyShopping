9a0c368250b55bec9edf0ab5ce1af71af1265447
leminhdung2701 <leminhdung_t64@hus.edu.vn>
2021-09-03 17:08:10 +0700

    new

---
+++ app/src/main/java/com/example/myshopping/Activity/PayActivity.java
@@ -0,0 +1,16 @@
+package com.example.myshopping.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myshopping.R;
+
+public class PayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pay);
+    }
+}
